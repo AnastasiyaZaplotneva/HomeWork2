e1262157c4c2046eacc7eeb7e645d904c230ea4d
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-21 13:56:17 +0300

    решили вторую задачу

---
+++ TaskTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if(number < 100)
+{
+    Console.WriteLine("У числа нет третьей цифры");
+}
+else
+{
+    while(number > 999)
+        number = number / 10;
+    Console.WriteLine(number % 10);
+}
